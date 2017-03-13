2400447c57f4c278a25d1299550d10637cd18637
Arthur Hotes <ahotes4@gmail.com>
2017-03-13 13:30:57 -0400

    Changed intro dialog window a bit

--- Forgotten Legend/RealCutScene.Designer.cs
+++ Forgotten Legend/RealCutScene.Designer.cs
@@ -40,13 +40,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.talkButton1o2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape3 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape4 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.SuspendLayout();
             // 
             // talkButton2
             // 
-            this.talkButton2.Location = new System.Drawing.Point(128, 240);
+            this.talkButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.talkButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.talkButton2.Location = new System.Drawing.Point(137, 348);
             this.talkButton2.Name = "talkButton2";
-            this.talkButton2.Size = new System.Drawing.Size(110, 23);
+            this.talkButton2.Size = new System.Drawing.Size(226, 25);
             this.talkButton2.TabIndex = 12;
             this.talkButton2.Text = "I was a fighter?";
             this.talkButton2.UseVisualStyleBackColor = true;
@@ -55,9 +62,11 @@
             // 
             // talkButton1
             // 
-            this.talkButton1.Location = new System.Drawing.Point(12, 240);
+            this.talkButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.talkButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.talkButton1.Location = new System.Drawing.Point(137, 306);
             this.talkButton1.Name = "talkButton1";
-            this.talkButton1.Size = new System.Drawing.Size(110, 23);
+            this.talkButton1.Size = new System.Drawing.Size(226, 23);
             this.talkButton1.TabIndex = 11;
             this.talkButton1.Text = "What is your name?";
             this.talkButton1.UseVisualStyleBackColor = true;
@@ -66,7 +75,7 @@
             // 
             // skipButton
             // 
-            this.skipButton.Location = new System.Drawing.Point(128, 359);
+            this.skipButton.Location = new System.Drawing.Point(240, 492);
             this.skipButton.Name = "skipButton";
             this.skipButton.Size = new System.Drawing.Size(110, 23);
             this.skipButton.TabIndex = 10;
@@ -77,9 +86,11 @@
             // 
             // talkButton3
             // 
-            this.talkButton3.Location = new System.Drawing.Point(12, 299);
+            this.talkButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.talkButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.talkButton3.Location = new System.Drawing.Point(137, 390);
             this.talkButton3.Name = "talkButton3";
-            this.talkButton3.Size = new System.Drawing.Size(110, 23);
+            this.talkButton3.Size = new System.Drawing.Size(226, 25);
             this.talkButton3.TabIndex = 9;
             this.talkButton3.Text = "Why am I here?";
             this.talkButton3.UseVisualStyleBackColor = true;
@@ -90,20 +101,22 @@
             // 
             this.richTextBox1.AutoWordSelection = true;
             this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 17);
+            this.richTextBox1.Location = new System.Drawing.Point(34, 12);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
-            this.richTextBox1.Size = new System.Drawing.Size(226, 215);
+            this.richTextBox1.Size = new System.Drawing.Size(423, 276);
             this.richTextBox1.TabIndex = 8;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // talkButton4
             // 
-            this.talkButton4.Location = new System.Drawing.Point(128, 299);
+            this.talkButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.talkButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.talkButton4.Location = new System.Drawing.Point(137, 433);
             this.talkButton4.Name = "talkButton4";
-            this.talkButton4.Size = new System.Drawing.Size(110, 23);
+            this.talkButton4.Size = new System.Drawing.Size(226, 27);
             this.talkButton4.TabIndex = 13;
             this.talkButton4.Text = "Can you help fight?";
             this.talkButton4.UseVisualStyleBackColor = true;
@@ -112,7 +125,7 @@
             // 
             // talkButton5
             // 
-            this.talkButton5.Location = new System.Drawing.Point(12, 359);
+            this.talkButton5.Location = new System.Drawing.Point(124, 492);
             this.talkButton5.Name = "talkButton5";
             this.talkButton5.Size = new System.Drawing.Size(110, 23);
             this.talkButton5.TabIndex = 14;
@@ -124,7 +137,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(150, 1);
+            this.label1.Location = new System.Drawing.Point(193, 525);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(97, 13);
             this.label1.TabIndex = 15;
@@ -132,9 +145,11 @@
             // 
             // talkButton1o2
             // 
-            this.talkButton1o2.Location = new System.Drawing.Point(12, 240);
+            this.talkButton1o2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.talkButton1o2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.talkButton1o2.Location = new System.Drawing.Point(137, 306);
             this.talkButton1o2.Name = "talkButton1o2";
-            this.talkButton1o2.Size = new System.Drawing.Size(110, 23);
+            this.talkButton1o2.Size = new System.Drawing.Size(226, 23);
             this.talkButton1o2.TabIndex = 16;
             this.talkButton1o2.Text = "Nice name";
             this.talkButton1o2.UseVisualStyleBackColor = true;
@@ -146,13 +161,63 @@
             this.timer1.Interval = 1500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape4,
+            this.lineShape3,
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(489, 541);
+            this.shapeContainer1.TabIndex = 17;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.Color.White;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 121;
+            this.lineShape1.X2 = 374;
+            this.lineShape1.Y1 = 380;
+            this.lineShape1.Y2 = 380;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.BorderColor = System.Drawing.Color.White;
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 122;
+            this.lineShape2.X2 = 374;
+            this.lineShape2.Y1 = 338;
+            this.lineShape2.Y2 = 338;
+            // 
+            // lineShape3
+            // 
+            this.lineShape3.BorderColor = System.Drawing.Color.White;
+            this.lineShape3.Name = "lineShape3";
+            this.lineShape3.X1 = 122;
+            this.lineShape3.X2 = 374;
+            this.lineShape3.Y1 = 424;
+            this.lineShape3.Y2 = 424;
+            // 
+            // lineShape4
+            // 
+            this.lineShape4.BorderColor = System.Drawing.Color.White;
+            this.lineShape4.Name = "lineShape4";
+            this.lineShape4.X1 = 122;
+            this.lineShape4.X2 = 374;
+            this.lineShape4.Y1 = 467;
+            this.lineShape4.Y2 = 467;
+            // 
             // RealCutScene
             // 
             this.AcceptButton = this.skipButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(250, 394);
+            this.ClientSize = new System.Drawing.Size(489, 541);
             this.ControlBox = false;
             this.Controls.Add(this.talkButton1o2);
             this.Controls.Add(this.label1);
@@ -163,6 +228,7 @@
             this.Controls.Add(this.skipButton);
             this.Controls.Add(this.talkButton3);
             this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.shapeContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "RealCutScene";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -184,5 +250,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button talkButton1o2;
         private System.Windows.Forms.Timer timer1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape4;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape3;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
     }
 }
