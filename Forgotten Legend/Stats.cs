99df0c0d2e267241f2c20bf397be274abdac37aa
JCeccoli <jacobceccoli@yahoo.com>
2017-03-15 12:36:09 -0400

    h

--- Forgotten Legend/Stats.cs
+++ Forgotten Legend/Stats.cs
@@ -8,11 +8,13 @@ namespace Forgotten_Legend
 {
     class Stats
     {
+        public int PlayerLevel;
         public int PlayerHpInt, PlayerMpInt;
         public int strPowerStat, defPowerStat;
+
         public int strPower, defPower, evadePower;
-        public int slashPower, bowPower, bolasPower;
-        public int firePower, freezePower;
+
+        public int slashPower, bowPower, bolasPower, firePower, freezePower;
         
 
     }
